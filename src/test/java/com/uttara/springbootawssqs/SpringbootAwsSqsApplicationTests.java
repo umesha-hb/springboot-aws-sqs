33bb0a17350d43268eccbee78d53a01aeac774d8
umesha-hb <umesha.kit@gmail.com>
2023-05-15 15:44:14 +0530

    frist commit

---
+++ src/test/java/com/uttara/springbootawssqs/SpringbootAwsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttara.springbootawssqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsSqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
